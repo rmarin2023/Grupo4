af429b0cad6ce14dc53128438ccbee23b9edbb9e
PatuzZ26 <169030513+PatuzZ26@users.noreply.github.com>
2024-06-06 14:03:59 +0200

    Update presentacion.java
    Hola

--- presentacion.java
+++ presentacion.java
@@ -6,6 +6,7 @@ public class presentacion
 	public static void main(String[] args) {
 		System.out.println("Hola mi nombre es Javier Pulido Alcón!");
 		System.out.println("Hola mi nombre es Carolina Rodríguez Muriel");
+		System.out.println("Hola mi nombre es Alberto Riscado Morato");
 	}
 
 	
